52ee299622b4bfd9048f34bfab61dbc26f35c8d4
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-03-14 16:59:28 +0200

    update api url

--- hooks/useProducts.jsx
+++ hooks/useProducts.jsx
@@ -3,7 +3,7 @@ import fetcher from "../libs/fetcher";
 // import dbConnect from "../utils/db/dbConnect";
 const useProducts = async () => {
   const { data, error, isLoading } = useSwr(
-    "https://ecommerce-backend-riewl4bil-mohammedramadan99.vercel.app/api/product",
+    "https://ecommerce-backend-5zcumxly0-mohammedramadan99.vercel.app/api/product",
     fetcher,
     {
       revalidateIfStale: false,
