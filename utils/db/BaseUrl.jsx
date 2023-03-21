a94a2ee6ff1c036229549191d18eea8eeb063053
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-03-21 15:00:52 +0200

    Update BaseUrl

--- utils/db/BaseUrl.jsx
+++ utils/db/BaseUrl.jsx
@@ -1,4 +1,4 @@
 const BaseUrl =
-  "https://ecommerce-backend-fedtlgjab-mohammedramadan99.vercel.app";
+  "https://ecommerce-backend-57p3u4ew5-mohammedramadan99.vercel.app";
 // "http://localhost:3001";
 export default BaseUrl;
